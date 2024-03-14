3e9e0584d9b229f7afa4e91871ba6edf5502b06c
LangYa466 <140164829+LangYa466@users.noreply.github.com>
2024-03-14 23:06:49 +0800

    Update KillAura.java

--- KillAura.java
+++ KillAura.java
@@ -2,7 +2,6 @@ package net.ccbluex.liquidbounce.features.module.modules.combat;
 
 import net.ccbluex.liquidbounce.RedBone;
 import net.ccbluex.liquidbounce.api.minecraft.client.IMinecraft;
-import net.ccbluex.liquidbounce.api.minecraft.client.entity.IEntity;
 import net.ccbluex.liquidbounce.api.minecraft.util.IAxisAlignedBB;
 import net.ccbluex.liquidbounce.event.EventTarget;
 import net.ccbluex.liquidbounce.event.Render3DEvent;
@@ -11,12 +10,12 @@ import net.ccbluex.liquidbounce.event.UpdateEvent;
 import net.ccbluex.liquidbounce.features.module.Module;
 import net.ccbluex.liquidbounce.features.module.ModuleCategory;
 import net.ccbluex.liquidbounce.features.module.ModuleInfo;
+import net.ccbluex.liquidbounce.features.module.modules.misc.Teams;
 import net.ccbluex.liquidbounce.utils.MinecraftInstance;
 import net.ccbluex.liquidbounce.utils.Rotation;
 import net.ccbluex.liquidbounce.utils.RotationUtils;
 import net.ccbluex.liquidbounce.utils.render.RenderUtils;
 import net.ccbluex.liquidbounce.utils.timer.MSTimer;
-import net.ccbluex.liquidbounce.utils.timer.TimeUtils;
 import net.ccbluex.liquidbounce.value.BoolValue;
 import net.ccbluex.liquidbounce.value.FloatValue;
 import net.ccbluex.liquidbounce.value.IntegerValue;
@@ -35,42 +34,53 @@ import java.util.List;
 
 @ModuleInfo(name = "Aura", description = "Auto Attack.", category = ModuleCategory.COMBAT)
 public class Aura extends Module {
-    IntegerValue maxCPSValue = new IntegerValue("MaxCPS",8, 1, 20);
-    IntegerValue minCPSValue = new IntegerValue("MinCPS",8, 1, 20);
+    IntegerValue maxCPSValue = new IntegerValue("MaxCPS", 8, 1, 20);
+    IntegerValue minCPSValue = new IntegerValue("MinCPS", 8, 1, 20);
+    ListValue getCPSMode = new ListValue("GetCPSMode", new String[]{"Random", "UP"}, "UP");
     FloatValue rangeValue = new FloatValue("Range", 3f, 3f, 6.0f);
     FloatValue throughWallsRangeValue = new FloatValue("ThroughWallsRange", 3f, 3f, 6.0f);
-    ListValue targetMode = new ListValue("TargetMode",  new String[]{"Single", "Multi"},"Single");
-    BoolValue noFoodValue = new BoolValue("NoFood",true);
-    ListValue rotationModeValue = new ListValue("RotationMode",new String[]{"Hyt","Silent", "None"},"Hyt");
+    ListValue targetMode = new ListValue("TargetMode", new String[]{"Single", "Multi"}, "Single");
+    BoolValue noFoodValue = new BoolValue("NoFood", true);
+    ListValue rotationModeValue = new ListValue("RotationMode", new String[]{"Normal", "None"}, "Hyt");
+    BoolValue silentRotationValue = new BoolValue("SilentRotation",true);
     FloatValue maxTurnSpeedValue = new FloatValue("MaxRotationSpeed", 180f, 1f, 180f);
     FloatValue minTurnSpeedValueValue = new FloatValue("MinRotationSpeed", 180f, 1f, 180f);
-    IntegerValue rotationKeepLengthValue = new IntegerValue("RotationKeepLength",0,0,20);
+    IntegerValue rotationKeepLengthValue = new IntegerValue("RotationKeepLength", 0, 0, 20);
     BoolValue predictValue = new BoolValue("Predict", true);
     FloatValue maxPredictSize = new FloatValue("MaxPredictSize", 1f, 0.1f, 5f);
     FloatValue minPredictSize = new FloatValue("MinPredictSize", 1f, 0.1f, 5f);
-    BoolValue outborderValue = new BoolValue("Outborder",false);
+    BoolValue outborderValue = new BoolValue("Outborder", false);
     BoolValue randomCenterValue = new BoolValue("RandomCenter", true);
+    BoolValue noAttackingValue = new BoolValue("NoAttacking", true);
+    BoolValue swingValue = new BoolValue("Swing", true);
+    BoolValue teamsValue = new BoolValue("Teams",true);
 
     List<Entity> targets = new ArrayList<>();
     Minecraft mc = mc2;
     IMinecraft mc3 = MinecraftInstance.mc;
-    MSTimer attackTimer = new MSTimer();
-    long attackDelay = 0L;
-    
+    int attacks;
+    MSTimer attackTimer;
+
     @EventTarget
     void onTick(TickEvent event) {
-        if(minCPSValue.get() > maxCPSValue.get()) maxCPSValue.set(minCPSValue.get());
+        if (minCPSValue.get() > maxCPSValue.get()) maxCPSValue.set(minCPSValue.get());
     }
-            
+
     @Override
     public void onDisable() {
         targets.clear();
-        attackTimer.reset();
+        attackTimer = null;
+    }
+
+    @Override
+    public void onEnable() {
+        targets.clear();
+        attackTimer = new MSTimer();
     }
 
     @EventTarget
     void onU(UpdateEvent e) {
-        if(noFoodValue.get() && mc.player.isHandActive() && mc.player.getHeldItem(EnumHand.MAIN_HAND).getItem() instanceof ItemFood) return;
+        if (noFoodValue.get() && mc.player.isHandActive() && mc.player.getHeldItem(EnumHand.MAIN_HAND).getItem() instanceof ItemFood) return;
         getTargets();
         attackTargets();
         targets.removeIf(entity -> entity.getDistance(mc.player) > rangeValue.get() || entity.isDead);
@@ -78,54 +88,51 @@ public class Aura extends Module {
 
     @EventTarget
     void onR3D(Render3DEvent e) {
-        if(targets.isEmpty()) return;
+        if (targets.isEmpty()) return;
 
-        if(targetMode.get().equals("Single")) {
-            RenderUtils.drawPlatform((IEntity) targets.get(0),new Color(37, 126, 255, 70));
+        if (targetMode.get().equals("Single")) {
+            RenderUtils.drawPlatform(targets.get(0), new Color(37, 126, 255, 70));
         }
 
-        if(targetMode.get().equals("Multi")) {
-            for(Entity entity : targets) {
-                RenderUtils.drawPlatform((IEntity) entity,new Color(37, 126, 255, 70));
-
+        if (targetMode.get().equals("Multi")) {
+            for (Entity entity : targets) {
+                RenderUtils.drawPlatform(entity, new Color(37, 126, 255, 70));
             }
         }
     }
 
     void getTargets() {
         for (Entity entity : mc.world.loadedEntityList) {
-            if(!entity.isDead && entity != mc.player && entity.getDistance(mc.player) <= rangeValue.get() && RedBone.moduleManager.getModule(KillAura.class).isEnemy((IEntity) entity)) targets.add(entity);
+            if (!entity.isDead && entity != mc.player && entity.getDistance(mc.player) <= rangeValue.get() || (!RedBone.moduleManager.getModule(Teams.class).isInYourTeam(entity) && teamsValue.get()))
+                targets.add(entity);
         }
     }
 
     void attackEntity(Entity entity) {
-        if(!attackTimer.hasTimePassed(attackDelay)) return;
-        mc.player.swingArm(EnumHand.MAIN_HAND);
+        if(!attackTimer.hasTimePassed(1000 / getCPS())) return;
+        if(swingValue.get()) mc.player.swingArm(EnumHand.MAIN_HAND);
         mc.getConnection().sendPacket(new CPacketUseEntity(entity));
-        attackTimer.reset();
-        attackDelay = TimeUtils.randomClickDelay(minCPSValue.get(), maxCPSValue.get());
+        attacks++;
     }
 
     void attackTargets() {
-        if(targets.isEmpty()) return;
-
-        if(targetMode.get().equals("Single")) {
+        if (targets.isEmpty() || (noAttackingValue.get() && mc.player.getAttackingEntity() != null)) return;
+        if (targetMode.get().equals("Single")) {
             attackEntity(targets.get(0));
-            doRotation(targets.get(0));
+            if(!rotationModeValue.get().equals("None")) doRotation(targets.get(0));
         }
 
-        if(targetMode.get().equals("Multi")) {
-            for(Entity entity : targets) {
+        if (targetMode.get().equals("Multi")) {
+            for (Entity entity : targets) {
                 attackEntity(entity);
-                doRotation(entity);
+                if(!rotationModeValue.get().equals("None")) doRotation(entity);
             }
         }
     }
 
     void doRotation(Entity entity) {
-        if (rotationModeValue.get().equals("None")) return;
 
-        if (rotationModeValue.get().equals("Hyt")) {
+        if (rotationModeValue.get().equals("Normal")) {
             AxisAlignedBB boundingBox = entity.getEntityBoundingBox();
             if (maxTurnSpeedValue.get() <= 0F) return;
             if (predictValue.get()) {
@@ -136,19 +143,38 @@ public class Aura extends Module {
                 );
             }
 
-            if (RotationUtils.lockView((IAxisAlignedBB) boundingBox, outborderValue.get() && !attackTimer.hasTimePassed(attackDelay / 2),
-                    randomCenterValue.get(), predictValue.get(), mc.player.getDistance(entity) < throughWallsRangeValue.get(), rangeValue.get()) == null)
+            if (RotationUtils.lockView((IAxisAlignedBB) boundingBox, outborderValue.get(), randomCenterValue.get(), predictValue.get(), mc.player.getDistance(entity) < throughWallsRangeValue.get(), rangeValue.get()) == null)
                 return;
 
             Rotation limitedRotation = RotationUtils.limitAngleChange(RotationUtils.serverRotation, RotationUtils.getNewRotations(RotationUtils.getCenter((IAxisAlignedBB) entity.getEntityBoundingBox()), false),
                     (float) (Math.random() * (maxTurnSpeedValue.get() - minTurnSpeedValueValue.get()) + minTurnSpeedValueValue.get()));
 
-            if (rotationModeValue.get().equals("Silent"))
+            if (silentRotationValue.get())
                 RotationUtils.setTargetRotation(limitedRotation, rotationKeepLengthValue.get());
             else
                 limitedRotation.toPlayer(mc3.getThePlayer());
 
         }
+    }
+
+    int getCPS() {
+        int maxCPS = maxCPSValue.get();
+        int minCPS = minCPSValue.get();
+
+        int cps = 0;
+        switch (getCPSMode.get()){
+            case "Random" :
+                cps = ((int) (Math.random() * (maxCPS - minCPS + 1)) + minCPS);
+                break;
+            case "UP" :
+                int upCPS = minCPS + attacks;
+                cps = Math.min(upCPS, maxCPS);
+            break;
+
+        }
+
+        return cps;
+
     }
 
 
